51f96c251efa361d0c8ee41a30294cc84ba8b961
Marek Hajdučák <hajducakmarek@gmail.com>
2025-03-13 14:14:14 +0100

    UI improvments in selection of categories

--- BudgetBuddy/BudgetBuddy/App/Features/Onboarding/OnboardingView.swift
+++ BudgetBuddy/BudgetBuddy/App/Features/Onboarding/OnboardingView.swift
@@ -3,7 +3,7 @@ import SwiftUI
 struct OnboardingView: View {
     @ObservedObject var presenter: OnboardingPresenter
     @State private var selectedTab = 0
-
+    
     var body: some View {
         TabView(selection: $selectedTab) {
             walletSelectionView
@@ -37,29 +37,57 @@ struct OnboardingView: View {
     }
     
     var categorySelectionView: some View {
-        VStack(alignment: .leading,  spacing: 16) {
+        VStack(alignment: .leading, spacing: 16) {
+            Spacer()
             Text("Select Categories you want to use:")
                 .font(.headline)
                 .padding(.horizontal, 20)
-            Text("Expenses")
-                .font(.headline)
-            ScrollView {
-                ForEach(presenter.defaultCategories.filter { $0.type == .expense }, id: \.id) { category in
-                    ChoosableCategoryView(presenter: presenter, category: category)
+            VStack(alignment: .leading, spacing: 10) {
+                Text("Expenses")
+                    .font(.headline)
+                LazyVGrid(
+                    columns: [
+                        GridItem(.flexible(), spacing: 10),
+                        GridItem(.flexible(), spacing: 10)
+                    ],
+                    spacing: 10
+                ) {
+                    ForEach(presenter.defaultCategories.filter { $0.type == .expense }, id: \.id) { category in
+                        ChoosableCategoryView(presenter: presenter, category: category)
+                            .frame(maxWidth: .infinity)
+                    }
                 }
             }
-            Text("Incomes")
-                .font(.headline)
-            ScrollView {
-                ForEach(presenter.defaultCategories.filter { $0.type == .income }, id: \.id) { category in
-                    ChoosableCategoryView(presenter: presenter, category: category)
+            .padding(.horizontal, 20)
+            
+            VStack(alignment: .leading, spacing: 10) {
+                Text("Incomes")
+                    .font(.headline)
+                LazyVGrid(
+                    columns: [
+                        GridItem(.flexible(), spacing: 10),
+                        GridItem(.flexible(), spacing: 10)
+                    ],
+                    spacing: 10
+                ) {
+                    ForEach(presenter.defaultCategories.filter { $0.type == .income }, id: \.id) { category in
+                        ChoosableCategoryView(presenter: presenter, category: category)
+                            .frame(maxWidth: .infinity)
+                    }
                 }
             }
+            .padding(.horizontal, 20)
             Spacer()
-            PrimaryButton(title: "Finish", isLoading: presenter.onbaordingIsLoading, isEnabled: presenter.isFinishOnbaordingEnabled) {
+            PrimaryButton(
+                title: "Finish",
+                isLoading: presenter.onbaordingIsLoading,
+                isEnabled: presenter.isFinishOnbaordingEnabled
+            ) {
                 presenter.completeOnboarding()
-            }.padding(.horizontal, 20)
-        }.authBaseView()
+            }
+            .padding(.horizontal, 20)
+            Spacer().frame(height: 25)
+        }
     }
 }
 
@@ -68,27 +96,20 @@ struct ChoosableCategoryView: View {
     var category: Category
 
     var body: some View {
-        HStack {
-            if presenter.selectedCategories.contains(where: { $0.id == category.id }) {
-                Image(systemName: "checkmark.circle.fill")
-                    .resizable()
-                    .foregroundColor(.blue)
-                    .frame(width: 20, height: 20)
-            } else {
-                ZStack(alignment: .center) {
-                    Circle()
-                        .fill(Color.gray)
-                        .frame(width: 20, height: 20)
-                    Circle()
-                        .fill(Color.accentColor)
-                        .frame(width: 18, height: 18)
-                }
-            }
-            Text(category.name).lineLimit(1)
-            Spacer()
+        let selected = presenter.selectedCategories.contains(where: { $0.id == category.id })
+        HStack(spacing: 8) {
+            Text(category.name)
+                .lineLimit(1)
+                .padding(8)
+                .fixedSize(horizontal: false, vertical: true)
+                .foregroundColor(selected ? .white : .primary)
+            Spacer(minLength: 0)
         }
-        .padding(.vertical, 4)
-        .padding(.horizontal, 20)
+        .frame(height: 36)
+        .background(
+            RoundedRectangle(cornerRadius: 18)
+                .fill(selected ? Color(.blue) : Color(.systemBackground).opacity(0.8))
+        )
         .tap {
             presenter.toggleCategorySelection(category)
         }
